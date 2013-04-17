ab24c9dad22eabb56e686c5b419b9425f46c1ef1
Jeroen Begyn <jeroen.begyn@gmail.com>
2013-04-17 01:36:22 +0100

    Change twitter job to work with twitter ruby gem to fix twitter API v1 deprecation errors

--- templates/project/jobs/twitter.rb
+++ templates/project/jobs/twitter.rb
@@ -1,17 +1,22 @@
+require 'twitter'
 require 'net/http'
 require 'json'
+
+Twitter.configure do |config|
+  config.consumer_key = 'YOUR_CONSUMER_KEY'
+  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
+  config.oauth_token = 'YOUR_OAUTH_TOKEN'
+  config.oauth_token_secret = 'YOUR_OAUTH_TOKEN_SECRET'
+end
 
 search_term = URI::encode('#todayilearned')
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
-  http = Net::HTTP.new('search.twitter.com')
-  response = http.request(Net::HTTP::Get.new("/search.json?q=#{search_term}"))
-  tweets = JSON.parse(response.body)["results"]
+  tweets = Twitter.search("#{search_term}").results
   if tweets
-    tweets.map! do |tweet| 
-      { name: tweet['from_user'], body: tweet['text'], avatar: tweet['profile_image_url_https'] }
+    tweets.map! do |tweet|
+      { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
     end
-  
     send_event('twitter_mentions', comments: tweets)
   end
 end
